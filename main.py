bb4ef1040c774f753c29511c7cb33a024452531d
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-22 12:45:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
