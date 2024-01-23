e7650c3b0bf1ccf29c995a0be5b07310eef3d222
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-23 16:40:47 +0100

    create Game class

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import pygame
+import sys
 
-pygame.init()
+WIDTH, HEIGHT = 1080, 720
+FPS = 60
 
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Your Game Title")
+        self.clock = pygame.time.Clock()
+        self.is_running = True
 
-pygame.quit()
+    def run(self):
+        while self.is_running:
+            self.handle_events()
+            pygame.display.flip()
+            self.clock.tick(FPS)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+
+
+if __name__ == "__main__":
+    game_instance = Game()
+    game_instance.run()
+    pygame.quit()
+    sys.exit()
