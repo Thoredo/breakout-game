eff685cedeb818b69c05e67c56e774349fb37d64
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-24 09:32:59 +0100

    Add main menu and switching between states

--- main.py
+++ main.py
@@ -9,20 +9,47 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Your Game Title")
+        pygame.display.set_caption("Breakout")
         self.clock = pygame.time.Clock()
         self.is_running = True
+
+        self.game_state_manager = GameStateManager("main menu")
+        self.main_menu = MainMenu(self.screen, self.game_state_manager)
+
+        self.states = {"main menu": self.main_menu}
 
     def run(self):
         while self.is_running:
             self.handle_events()
-            pygame.display.flip()
             self.clock.tick(FPS)
+
+            self.states[self.game_state_manager.get_state()].run()
+            pygame.display.update()
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
+
+
+class MainMenu:
+    def __init__(self, display, game_state_manager):
+        self.display = display
+        self.gamestatemanager = game_state_manager
+
+    def run(self):
+        self.display.fill("black")
+
+
+class GameStateManager:
+    def __init__(self, current_state):
+        self.current_state = current_state
+
+    def get_state(self):
+        return self.current_state
+
+    def set_state(self, state):
+        self.current_state = state
 
 
 if __name__ == "__main__":
