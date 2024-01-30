614f87931a39343d3c302f11b1a3a808cdd13217
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-30 13:01:51 +0100

    Main menu UI changes to pygame buttons

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import pygame_menu
 import sys
 
 WIDTH, HEIGHT = 1080, 720
@@ -14,16 +13,22 @@ class Game:
         self.clock = pygame.time.Clock()
         self.is_running = True
 
+        pygame.font.init()
+        self.my_font = pygame.font.SysFont("Arial", 30, bold=True)
+
         self.game_state_manager = GameStateManager("main menu")
         self.main_menu = MainMenu(self.screen, self.game_state_manager)
+        self.instructions = InstructionsPage(self.screen, self.game_state_manager)
 
-        self.states = {"main menu": self.main_menu}
+        self.states = {
+            "main menu": self.main_menu,
+            "instructions": self.instructions,
+        }
 
     def run(self):
         while self.is_running:
             self.handle_events()
             self.clock.tick(FPS)
-
             self.states[self.game_state_manager.get_state()].run()
             pygame.display.update()
 
@@ -43,20 +48,36 @@ class MainMenu:
         self.draw()
 
     def draw(self):
-        menu = pygame_menu.Menu(
-            title="Breakout",
-            width=1080,
-            height=720,
-            theme=pygame_menu.themes.THEME_DARK,
-        )
-        play_button = menu.add.button("Play")
-        instructions_button = menu.add.button("Instructions")
-        highscores_button = menu.add.button("Highscores")
-        quit_button = menu.add.button("Quit")
-        menu.mainloop(self.display)
+        logo = pygame.image.load("logo.png")
+        self.display.blit(logo, (273, 145))
+
+        pygame.draw.rect(self.display, "black", (490, 300, 100, 50))
+        play_text = game_instance.my_font.render("Play", True, "white")
+        self.display.blit(play_text, (515, 307))
+
+        pygame.draw.rect(self.display, "black", (450, 360, 200, 50))
+        instructions_text = game_instance.my_font.render("Instructions", True, "white")
+        self.display.blit(instructions_text, (480, 366))
+
+        pygame.draw.rect(self.display, "black", (490, 420, 100, 50))
+        quit_text = game_instance.my_font.render("Quit", True, "white")
+        self.display.blit(quit_text, (515, 426))
 
     def open_instructions(self):
-        pass
+        self.gamestatemanager.set_state("instructions")
+
+
+class InstructionsPage:
+    def __init__(self, display, game_state_manager):
+        self.display = display
+        self.gamestatemanager = game_state_manager
+
+    def run(self):
+        self.display.fill("black")
+        self.draw()
+
+    def draw(self):
+        self.instructions_page.mainloop(self.display)
 
 
 class GameStateManager:
