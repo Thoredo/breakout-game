1509b818ca1a99981589f97bb5d141b79d4cf1ee
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-30 13:34:05 +0100

    Change menu buttons color on hover and open instructions page

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ class Game:
         pygame.display.set_caption("Breakout")
         self.clock = pygame.time.Clock()
         self.is_running = True
+        self.mouse = (0, 0)
+        self.mouse_clicked = (False, False, False)
 
         pygame.font.init()
-        self.my_font = pygame.font.SysFont("Arial", 30, bold=True)
+        self.menu_font = pygame.font.SysFont("Arial", 30, bold=True)
 
         self.game_state_manager = GameStateManager("main menu")
         self.main_menu = MainMenu(self.screen, self.game_state_manager)
@@ -30,6 +32,8 @@ class Game:
             self.handle_events()
             self.clock.tick(FPS)
             self.states[self.game_state_manager.get_state()].run()
+            self.mouse = pygame.mouse.get_pos()
+            self.mouse_clicked = pygame.mouse.get_pressed()
             pygame.display.update()
 
     def handle_events(self):
@@ -51,16 +55,34 @@ class MainMenu:
         logo = pygame.image.load("logo.png")
         self.display.blit(logo, (273, 145))
 
+        # Play Button
         pygame.draw.rect(self.display, "black", (490, 300, 100, 50))
-        play_text = game_instance.my_font.render("Play", True, "white")
+        if 490 < game_instance.mouse[0] < 590 and 300 < game_instance.mouse[1] < 350:
+            play_text = game_instance.menu_font.render("Play", True, "red")
+        else:
+            play_text = game_instance.menu_font.render("Play", True, "white")
         self.display.blit(play_text, (515, 307))
 
+        # Instructions button
         pygame.draw.rect(self.display, "black", (450, 360, 200, 50))
-        instructions_text = game_instance.my_font.render("Instructions", True, "white")
+        if 450 < game_instance.mouse[0] < 650 and 360 < game_instance.mouse[1] < 410:
+            instructions_text = game_instance.menu_font.render(
+                "Instructions", True, "red"
+            )
+            if game_instance.mouse_clicked[0] == True:
+                self.open_instructions()
+        else:
+            instructions_text = game_instance.menu_font.render(
+                "Instructions", True, "white"
+            )
         self.display.blit(instructions_text, (480, 366))
 
+        # Quit Button
         pygame.draw.rect(self.display, "black", (490, 420, 100, 50))
-        quit_text = game_instance.my_font.render("Quit", True, "white")
+        if 490 < game_instance.mouse[0] < 590 and 420 < game_instance.mouse[1] < 470:
+            quit_text = game_instance.menu_font.render("Quit", True, "red")
+        else:
+            quit_text = game_instance.menu_font.render("Quit", True, "white")
         self.display.blit(quit_text, (515, 426))
 
     def open_instructions(self):
@@ -77,7 +99,7 @@ class InstructionsPage:
         self.draw()
 
     def draw(self):
-        self.instructions_page.mainloop(self.display)
+        pass
 
 
 class GameStateManager:
