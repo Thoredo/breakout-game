94ffeed087721e2928350d57652bebaef2eea1d5
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-02-02 16:16:18 +0100

    Move Paddle to own file

---
+++ game_elements/paddle.py
@@ -0,0 +1,28 @@
+import pygame
+
+PADDLE_SPEED = 4
+
+
+class Paddle:
+    def __init__(self, display):
+        self.display = display
+        self.width = 80
+        self.height = 10
+        self.x_pos = 500
+        self.y_pos = 680
+
+    def draw_paddle(self):
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
+        pygame.draw.rect(
+            self.display,
+            "yellow",
+            (self.x_pos, self.y_pos, self.width, self.height),
+        )
+
+    def move_left(self):
+        if self.x_pos > 0:
+            self.x_pos -= PADDLE_SPEED
+
+    def move_right(self):
+        if self.x_pos < 1000:
+            self.x_pos += PADDLE_SPEED
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from game_interface.game_state_manager import GameStateManager
 from game_interface.instructions_page import InstructionsPage
 from game_interface.game_screen import GameScreen
 from game_interface.main_menu import MainMenu
+from game_elements.paddle import Paddle
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 1080, 720
 FPS = 60
-PADDLE_SPEED = 4
 BALL_SPEED_X = 4
 BALL_SPEED_Y = -4
 
@@ -68,31 +68,6 @@ class Game:
                 self.ball.x_pos = self.paddle.x_pos + 40
         if keys[pygame.K_UP]:
             self.ball.on_paddle = False
-
-
-class Paddle:
-    def __init__(self, display):
-        self.display = display
-        self.width = 80
-        self.height = 10
-        self.x_pos = 500
-        self.y_pos = 680
-
-    def draw_paddle(self):
-        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
-        pygame.draw.rect(
-            self.display,
-            "yellow",
-            (self.x_pos, self.y_pos, self.width, self.height),
-        )
-
-    def move_left(self):
-        if self.x_pos > 0:
-            self.x_pos -= PADDLE_SPEED
-
-    def move_right(self):
-        if self.x_pos < 1000:
-            self.x_pos += PADDLE_SPEED
 
 
 class Ball:
