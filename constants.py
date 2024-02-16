baf3465b4eb7142ecbd1a5f07cf06b74c72c2608
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-02-16 14:49:35 +0100

    Add boost names

--- constants.py
+++ constants.py
@@ -15,3 +15,13 @@ SPEED_BOOSTS_NUMBER = 0.5
 PADDLE_SPEED = 4
 PADDLE_STARTING_X = 500
 PADDLE_STARTING_Y = 680
+
+# Boost names
+EXTEND_TYPE = "extend paddle"
+SHRINK_TYPE = "shrink paddle"
+FASTER_TYPE = "faster ball"
+SLOWER_TYPE = "slower ball"
+LIFE_TYPE = "extra life"
+POINTS_TYPE = "point boost"
+BALL_TYPE = "extra ball"
+SHOOT_TYPE = "paddle shoots"
